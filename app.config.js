0cd3a6a08ef629b80ce4b9ca6db761e4bd61c679
gunny <qkr133456@gmail.com>
2026-02-18 15:17:26 +0900

    02181517

--- app.config.js
+++ app.config.js
@@ -37,7 +37,7 @@ module.exports = ({ config }) => {
       ...config.expo,
       name: currentEnv.name,
       slug: 'gns-hermit-comm',
-      version: '1.1.0',
+      version: '1.1.1',
       orientation: 'portrait',
       icon: './assets/icon.png',
       scheme: currentEnv.scheme,
